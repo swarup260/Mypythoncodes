ba1c490233146b1b536399f5bd962bf618b64446
swarup260 <swarup260@gmail.com>
2018-03-04 23:23:17 +0530

    Flask Boilerplate starter kit

---
+++ flask_Boilerplate/app.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('siginup.html')
+
+''' Run the flask application  
+Debug mode
+'''
+if __name__ == '__main__':
+    app.run(debug= True)
