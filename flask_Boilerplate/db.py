a3820f06ab3fbe0611907ec45a095598d46b91cc
swarup260 <swarup260@gmail.com>
2018-05-30 23:48:14 +0530

    unuse code

--- flask_Boilerplate/db.py
+++
@@ -1,2 +0,0 @@
-from flask import Flask
-from flask_mysqldb import MySQL
--- flask_Boilerplate/testing_code.py
+++
@@ -1,7 +0,0 @@
-@app.route('/signin', methods=['GET', 'POST'])
-def signin():
-    login = Signupform(request.Form)
-    if(request.method == 'POST' and login.validate()):
-        flash('Thanks for registering')
-        return redirect(url_for('signup'))
-    return render_template('signin.html', form=login)
